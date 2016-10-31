91aa99741c47bf6d679345555f4c2b9ff1bb5538
Joseph Adams <jdfadams@gmail.com>
2016-10-31 07:15:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Recipes</title>
+<meta http-equiv="Content-Type" content="application/xhtml+xml;charset=utf-8" />
+</head>
+<body>
+
+<center>
+<h1>Recipes</h1><br />
+
+<img src="https://drive.google.com/open?id=0B2yL7VqECgSQYjFOdDFqbnRVSXc" /><br />k=8<br />
+
+<img src="https://drive.google.com/open?id=0B2yL7VqECgSQLU03aE9hWG5yVnc" /><br />k=10<br />
+
 <?php
+  echo "Hello World!";
+  ?>
 
-echo "Hello World!";
-
-?>
+</body>
+</html>
